3b97ea753277ae657f3c38236d4ad3aef350bd29
Postal5 <76649655+m-zagornyak@users.noreply.github.com>
2022-05-26 01:11:33 +0300

    w

--- loader.py
+++ loader.py
@@ -1,8 +1,7 @@
 from aiogram import Bot, types, Dispatcher
-from aiogram.contrib.fsm_storage.memory import MemoryStorage
 
-from data import config
+from data.config import BOT_TOKEN
 
-bot = Bot(token=config.BOT_TOKEN, parse_mode=types.ParseMode.HTML)
+bot = Bot(token=BOT_TOKEN, parse_mode=types.ParseMode.HTML)
 
 dp = Dispatcher(bot)
